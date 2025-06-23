4227a068cf0a3a31204a8a073002cdb85338e6f4
Diego1865 <dibaos1b@gmail.com>
2025-06-22 20:45:09 -0600

    Actualización de forma ataque voraz

--- agente_batalla.py
+++ agente_batalla.py
@@ -1,11 +1,14 @@
 import random
-from config import TAMANO_TABLERO
+from config import TAMANO_TABLERO, BARCOS
 
 class AgenteBattleship:
     def __init__(self):
         self.disparos_realizados = set()
         self.en_caceria = True
         self.objetivos_pendientes = []
+        self.aciertos_actuales = []
+        self.direccion_confirmada = None
+        self.barcos_restantes = {v["inicial"]: v["tamaño"] for v in BARCOS.values()}
 
     def siguiente_disparo(self, tablero_objetivo, tablero_disparo):
         if self.en_caceria:
@@ -22,6 +25,7 @@ class AgenteBattleship:
                 resultado = self.evaluar_disparo(tablero_objetivo, tablero_disparo, fila, col)
                 if resultado == "acierto":
                     self.en_caceria = False
+                    self.aciertos_actuales = [(fila, col)]
                     self.objetivos_pendientes = self.vecinos_validos(fila, col, tablero_disparo)
                 return fila, col, resultado
 
@@ -32,19 +36,43 @@ class AgenteBattleship:
                 continue
             self.disparos_realizados.add((fila, col))
             resultado = self.evaluar_disparo(tablero_objetivo, tablero_disparo, fila, col)
+
             if resultado == "acierto":
-                self.objetivos_pendientes += self.vecinos_validos(fila, col, tablero_disparo)
+                self.aciertos_actuales.append((fila, col))
+                if len(self.aciertos_actuales) >= 2 and not self.direccion_confirmada:
+                    self.direccion_confirmada = self.detectar_direccion()
+                if self.direccion_confirmada:
+                    self.extender_en_direccion(tablero_disparo)
+                else:
+                    self.objetivos_pendientes += self.vecinos_validos(fila, col, tablero_disparo)
             else:
                 if not self.objetivos_pendientes:
-                    self.en_caceria = True
+                    self.resetear_caceria()
             return fila, col, resultado
 
-        self.en_caceria = True
+        self.resetear_caceria()
         return self.modo_caceria(tablero_objetivo, tablero_disparo)
 
     def evaluar_disparo(self, tablero_objetivo, tablero_disparo, fila, col):
-        if tablero_objetivo[fila][col] in ["A", "B", "C", "S", "F"]:
+        celda = tablero_objetivo[fila][col]
+        if celda in self.barcos_restantes:
             tablero_disparo[fila][col] = "X"
+
+            # ¿El barco fue hundido?
+            hundido = True
+            for f in range(TAMANO_TABLERO):
+                for c in range(TAMANO_TABLERO):
+                    if tablero_objetivo[f][c] == celda and tablero_disparo[f][c] != "X":
+                        hundido = False
+                        break
+                if not hundido:
+                    break
+
+            if hundido:
+                self.barcos_restantes.pop(celda, None)
+                self.resetear_caceria()
+            else:
+                self.en_caceria = False  # Aún quedan partes del barco por encontrar
             return "acierto"
         else:
             tablero_disparo[fila][col] = "O"
@@ -58,3 +86,29 @@ class AgenteBattleship:
                 if tablero_disparo[nf][nc] == "_":
                     vecinos.append((nf, nc))
         return vecinos
+
+    def detectar_direccion(self):
+        if len(self.aciertos_actuales) < 2:
+            return None
+        (f1, c1), (f2, c2) = self.aciertos_actuales[-2:]
+        if f1 == f2:
+            return (0, 1) if c2 > c1 else (0, -1)
+        elif c1 == c2:
+            return (1, 0) if f2 > f1 else (-1, 0)
+        return None
+
+    def extender_en_direccion(self, tablero_disparo):
+        if not self.direccion_confirmada:
+            return
+        df, dc = self.direccion_confirmada
+        for base in self.aciertos_actuales:
+            for signo in [1, -1]:
+                nf, nc = base[0] + df * signo, base[1] + dc * signo
+                if 0 <= nf < TAMANO_TABLERO and 0 <= nc < TAMANO_TABLERO:
+                    if tablero_disparo[nf][nc] == "_" and (nf, nc) not in self.disparos_realizados:
+                        self.objetivos_pendientes.append((nf, nc))
+
+    def resetear_caceria(self):
+        self.en_caceria = True
+        self.aciertos_actuales.clear()
+        self.direccion_confirmada = None
